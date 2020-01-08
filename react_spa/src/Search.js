a5d98dc9b93166aedfe63c3d597d1a3970103522
maurafortino <maurafortino@gmail.com>
2020-01-07 19:29:51 -0500

    added the SearchForm component

--- react_spa/src/Search.js
+++ react_spa/src/Search.js
@@ -1,49 +1,14 @@
-import React, { useState } from "react";
-import Container from "../src/components/Container";
-import Row from "../src/components/Row";
-import Col from "../src/components/Col";
+import React from "react";
+import SearchForm from "./components/Search";
  
-const Search = () => {
-  const [search, setSearch] = useState();
-
-  const handleSumbit = e => {
-      e.preventDefeault();
-  };
-
-  return (
-    <div>
-        <form onSumbit={handleSumbit}>
-            <Container>
-                <Row className="form-group">
-                    <Col size="12">
-                        <input
-                            className="form-control"
-                            type="text"
-                            placeholder="search for book here"
-                            name="search"
-                            onChange={search => setSearch(search.target.value)}
-                        />
-                    </Col>
-                </Row>
-                <button className="btn btn-success" type="submit">Submit</button>
-            </Container>
-        </form>
-
-        <Container>
-            <Row>
-                <Col size="12">
-                    <div className="search-results">
-                        {search}
-                    </div>
-                </Col>
-            </Row>
-        </Container>
-    </div>
-);
-};
+const Search = () =>{
+    return (
+      <div>
+        <h2>Search</h2>
+        <SearchForm />
+      </div>
+    );
+}
  
 export default Search;
-
-
-  
 
